ed802d930334f762c30ebaaa13ecdb1402e64551
PhilipBoyle <phlpbyl@gmail.com>
2016-12-01 20:26:31 +0000

    Added Basic HTML

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Homepage"
+    return render_template("converter.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
